637322ee26c20a6aee46e33af3021b43d257e633
Just-Ven <alnurbaisar@gmail.com>
2024-09-17 16:46:01 +0500

    first lab

---
+++ lab 1.sql
@@ -0,0 +1,18 @@
+create database lab1;
+create table users (
+    id serial,
+    firstname varchar(50),
+    lastname varchar(50)
+);
+
+alter table users add isadmin integer;
+alter table users alter column isadmin type boolean using isadmin::boolean;
+alter table users alter column isadmin set default false;
+alter table users add primary key (id);
+
+create table tasks (
+    id serial,    name varchar(50),
+    user_id integer);
+
+drop table tasks;
+drop database lab1;
